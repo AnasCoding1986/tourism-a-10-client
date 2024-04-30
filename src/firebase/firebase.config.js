32485dfc0870a3b9cbe9e520b634a3aa4d85f6cf
AnasCoding1986 <anassust.scw@gmail.com>
2024-04-30 06:52:14 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoqiszPwVKuuiXzmhJl1cfhgWDztdpNjE",
-  authDomain: "tourism-6b17e.firebaseapp.com",
-  projectId: "tourism-6b17e",
-  storageBucket: "tourism-6b17e.appspot.com",
-  messagingSenderId: "971826222017",
-  appId: "1:971826222017:web:da67dae69e5ad668441540"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
